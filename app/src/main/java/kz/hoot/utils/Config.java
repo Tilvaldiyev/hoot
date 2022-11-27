09a0ae4b79985d4c303b1d7616170daa429f2f3e
Dastan <101543359+dastunning@users.noreply.github.com>
2022-11-27 18:52:30 +0600

    Casts (#3)
    Conflicts Resolved

--- app/src/main/java/kz/hoot/utils/Config.java
+++ app/src/main/java/kz/hoot/utils/Config.java
@@ -1,5 +1,6 @@
 package kz.hoot.utils;
 
 public class Config {
-    public static final String BASE_URL = "http://tauekelinspector.kz:9090/hoot/";
+//    public static final String BASE_URL = "http://tauekelinspector.kz:9090/hoot/";
+    public static final String BASE_URL = "http://172.16.81.30:9090/hoot/";
 }
